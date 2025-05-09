59b8a2eaccb3e404913822ec54462b01a8b17980
iaanngabriell <iaanngabriell@gmail.com>
2025-05-09 17:05:22 -0300

    version 6

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.listaTarefa, name='lista-tarefas'),  
+    path('', views.listaTarefa, name='lista-tarefas'),
+    path('tarefa/<int:id>', views.tarefaView, name='tarefa-view'),
+    path('novaTarefa/', views.novaTarefa, name='nova-tarefa'),
+    path('edit/<int:id>', views.editTarefa, name='edit-tarefa'),
+    path('delete/<int:id>', views.deleteTarefa, name='delete-tarefa'),
 ]
--- tarefas/views.py
+++ tarefas/views.py
@@ -1,6 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Tarefas
+from .forms import TarefaForm
 
 # Create your views here.
 
 def listaTarefa(request):
-    return render(request, 'tarefas/list.html')
+    tarefas_list = Tarefas.objects.all().order_by('-created_at')
+    return render(request, 'tarefas/list.html', {'tarefas':tarefas_list})
+
+def tarefaView(request, id):
+    tarefa = get_object_or_404(Tarefas, pk=id)
+    return render(request, 'tarefas/tarefa.html', {'tarefa':tarefa})
+
+def novaTarefa(request):
+    if request.method == 'POST':
+        form = TarefaForm(request.POST)
+
+        if form.is_valid():
+            tarefa = form.save()
+            tarefa.save()
+            return redirect('/')
+        
+    else: 
+        form = TarefaForm()
+        return render(request, 'tarefas/addTarefa.html', {'form':form})
+
+def editTarefa(request, id):
+    tarefa = get_object_or_404(Tarefas, pk=id)
+    form = TarefaForm(instance=tarefa)
+
+    if(request.method == 'POST'):
+        form = TarefaForm(request.POST, instance=tarefa)
+
+        if(form.is_valid()):
+            tarefa.save()
+            return redirect('/')
+        else:
+            return render(request, 'tarefas/editTarefa.html', {'form':form, 'tarefa':tarefa})
+    
+    else:
+        return render(request, 'tarefas/editTarefa.html', {'form':form, 'tarefa':tarefa})
+    
+def deleteTarefa(request, id):
+    tarefa = get_object_or_404(Tarefas, pk=id)
+    tarefa.delete()
+    return redirect('/')
+
